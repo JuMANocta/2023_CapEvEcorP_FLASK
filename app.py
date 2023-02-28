65179bd7344eac7f6e2c902c5964593e2bd4f623
dev <julien.mangeat@mail-formateur.net>
2023-02-28 16:11:18 +0100

    ajout d'un formulaire de connexion

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, redirect, abort, jsonify, url_for, request, send_from_directory
+from flask import Flask, render_template, redirect, abort, jsonify, url_for, request, send_from_directory, flash
 from flask_bootstrap import Bootstrap
 from models.utilisateur import Utilisateur, db_utilisateur
 from models.quiz import Quiz, db_quiz
 from models.quiz_new_form import QuizNewForm
+from models.connexion_form import ConnexionForm
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+from werkzeug.security import generate_password_hash
+
 
 app = Flask(__name__, static_folder='static')
 app.config.from_pyfile('config.py')
@@ -15,7 +19,13 @@ with app.app_context():
     db_utilisateur.create_all()
     db_quiz.create_all()
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'connexion'
+login_manager.login_message = 'Veuillez vous connecter pour accéder à cette page'
+
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
@@ -55,9 +65,10 @@ def json():
     data = {'key': 'value', 'key2': 'value2', 'key3': 'value3'}
     return jsonify(data)
 
-@app.route('/setutilisateur/<nom>/<mot_de_passe>/<role>')
-def utilisateur(nom, mot_de_passe, role):
-    utilisateur = Utilisateur(nom=nom, mot_de_passe=mot_de_passe, role=role)
+@app.route('/setutilisateur/<nom>/<mot_de_passe>/<email>/<role>')
+def utilisateur(nom, mot_de_passe,email, role):
+    mot_de_passe_hache = generate_password_hash(mot_de_passe)
+    utilisateur = Utilisateur(nom=nom, mot_de_passe=mot_de_passe_hache, email=email,role=role)
     db_utilisateur.session.add(utilisateur)
     db_utilisateur.session.commit()
     return 'Utilisateur ajouté'
@@ -70,7 +81,7 @@ def get_utilisateur(id):
 @app.route('/getallutilisateurs')
 def get_all_utilisateurs():
     utilisateurs = Utilisateur.query.all()
-    return f'Utilisateurs {[(utilisateur.nom, utilisateur.mot_de_passe, utilisateur.role) for utilisateur in utilisateurs]}'
+    return f'Utilisateurs {[(utilisateur.nom, utilisateur.mot_de_passe, utilisateur.email, utilisateur.role) for utilisateur in utilisateurs]}'
 
 @app.route('/htmlutilisateurs')
 def html_utilisateurs():
@@ -122,9 +133,31 @@ def quiz_delete(id):
     db_quiz.session.commit()
     return redirect(url_for('quiz'))
 
-# @app.route('/img/<path:path>')
-# def send_img(path):
-#     return send_from_directory('static/img', path)
+@app.route('/connexion', methods=['GET', 'POST'])
+def connexion():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = ConnexionForm()
+    if form.validate_on_submit():
+        utilisateur = Utilisateur.query.filter_by(nom=form.login.data).first()
+        if utilisateur is None or not utilisateur.check_password(form.mot_de_passe.data):
+            flash('Email ou mot de passe invalide')
+            return redirect(url_for('connexion'))
+
+        login_user(utilisateur, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+    return render_template('connexion/connexion.html', form=form)
+
+@app.route('/deconnexion')
+def deconnexion():
+    logout_user()
+    return redirect(url_for('index'))
+
+@login_manager.user_loader
+def load_user(id):
+    return Utilisateur.query.get(int(id))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- models/utilisateur.py
+++ models/utilisateur.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db_utilisateur = SQLAlchemy()
 
@@ -6,7 +7,23 @@ class Utilisateur(db_utilisateur.Model):
     id = db_utilisateur.Column(db_utilisateur.Integer, primary_key=True)
     nom = db_utilisateur.Column(db_utilisateur.String(80), unique=True, nullable=False)
     mot_de_passe = db_utilisateur.Column(db_utilisateur.String(80), nullable=False)
+    email = db_utilisateur.Column(db_utilisateur.String(120), unique=True, nullable=False)
     role = db_utilisateur.Column(db_utilisateur.String(80), nullable=False)
 
+    def check_password(self, password):
+        return check_password_hash(self.mot_de_passe, password)
+    
+    def is_active(self):
+        return True
+    
+    def get_id(self):
+        return str(self.id)
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+    
     def __repr__(self):
         return f'<Utilisateur {self.nom}>'
