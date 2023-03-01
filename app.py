3febff82049519773350f55e3f1462f5807148c8
dev <julien.mangeat@mail-formateur.net>
2023-03-01 13:48:02 +0100

    page de création d'un user opé

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from models.quiz_new_form import QuizNewForm
 from models.connexion_form import ConnexionForm
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash
+from models.crea_user_form import NouvelUtilisateurForm
 
 
 app = Flask(__name__, static_folder='static')
@@ -24,74 +25,92 @@ login_manager.init_app(app)
 login_manager.login_view = 'connexion'
 login_manager.login_message = 'Veuillez vous connecter pour accéder à cette page'
 
+
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
 
+
 @app.route('/bootstrap')
 def bootstrap():
     return render_template('bootstrap.html')
+
 
 @app.route('/salut')
 def salut():
     return 'Salut'
 
+
 @app.route('/salut/<nom>')
 def test_name(nom):
-    from datetime import datetime # importation de datetime
-    maintenant = datetime.now() # récupération de la date et de l'heure actuelle
-    jour = maintenant.day # récupération du jour
-    jours_de_la_semaine_fr = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'] # création d'une liste des jours de la semaine en français
-    jour_semaine = jours_de_la_semaine_fr[maintenant.weekday()] # récupération du jour de la semaine en français
-    mois = maintenant.month # récupération du mois
-    annee = maintenant.year # récupération de l'année
-    return render_template('salut.html', nom=nom, jour=jour, mois=mois, annee=annee ,jour_semaine=jour_semaine)
+    from datetime import datetime  # importation de datetime
+    maintenant = datetime.now()  # récupération de la date et de l'heure actuelle
+    jour = maintenant.day  # récupération du jour
+    # création d'une liste des jours de la semaine en français
+    jours_de_la_semaine_fr = [
+        'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+    # récupération du jour de la semaine en français
+    jour_semaine = jours_de_la_semaine_fr[maintenant.weekday()]
+    mois = maintenant.month  # récupération du mois
+    annee = maintenant.year  # récupération de l'année
+    return render_template('salut.html', nom=nom, jour=jour, mois=mois, annee=annee, jour_semaine=jour_semaine)
+
 
 @app.route('/google')
 def google():
     return redirect('https://google.com')
 
+
 @app.route('/401')
 def error_401():
     abort(401)
 
+
 @app.errorhandler(404)
 def error_404(error):
     return ("404"), 404
+
 
 @app.route('/json')
 def json():
     data = {'key': 'value', 'key2': 'value2', 'key3': 'value3'}
     return jsonify(data)
 
+
 @app.route('/setutilisateur/<nom>/<mot_de_passe>/<email>/<role>')
-def utilisateur(nom, mot_de_passe,email, role):
-    mot_de_passe_hache = generate_password_hash(mot_de_passe)
-    utilisateur = Utilisateur(nom=nom, mot_de_passe=mot_de_passe_hache, email=email,role=role)
+def utilisateur(nom, mot_de_passe, email, role):
+    mot_de_passe_hash = generate_password_hash(mot_de_passe)
+    utilisateur = Utilisateur(
+        nom=nom, mot_de_passe=mot_de_passe_hash, email=email, role=role)
     db_utilisateur.session.add(utilisateur)
     db_utilisateur.session.commit()
     return 'Utilisateur ajouté'
+
 
 @app.route('/getutilisateur/<id>')
 def get_utilisateur(id):
     utilisateur = Utilisateur.query.get(id)
     return f'Utilisateur {utilisateur.nom, utilisateur.mot_de_passe}'
 
+
 @app.route('/getallutilisateurs')
 def get_all_utilisateurs():
     utilisateurs = Utilisateur.query.all()
     return f'Utilisateurs {[(utilisateur.nom, utilisateur.mot_de_passe, utilisateur.email, utilisateur.role) for utilisateur in utilisateurs]}'
+
 
 @app.route('/htmlutilisateurs')
 def html_utilisateurs():
     utilisateurs = Utilisateur.query.all()
     return render_template('utilisateurs.html', utilisateurs=utilisateurs)
 
+
 @app.route('/quiz')
 def quiz():
     quiz = Quiz.query.all()
     return render_template('quiz/quiz.html', quiz=quiz)
+
 
 @app.route('/quiz_new', methods=['GET', 'POST'])
 def quiz_new():
@@ -99,10 +118,12 @@ def quiz_new():
     if request.method == 'POST':
         quiz = Quiz()
         form.populate_obj(quiz)
-        db_quiz.session.add(quiz) # Ajout de l'objet dans la session de la base de données
+        # Ajout de l'objet dans la session de la base de données
+        db_quiz.session.add(quiz)
         db_quiz.session.commit()
         return redirect(url_for('quiz'))
     return render_template('quiz/quiz_new.html', form=form)
+
 
 @app.route('/quiz/<int:id>', methods=['GET', 'POST'])
 def quiz_id(id):
@@ -114,7 +135,8 @@ def quiz_id(id):
             return render_template('quiz/quiz_win.html')
         else:
             return render_template('quiz/quiz_lose.html')
-    return render_template('quiz/quiz_id.html',quiz=quiz)
+    return render_template('quiz/quiz_id.html', quiz=quiz)
+
 
 @app.route('/quiz_modif/<int:id>', methods=['GET', 'POST'])
 def quiz_modif(id):
@@ -126,12 +148,14 @@ def quiz_modif(id):
         return redirect(url_for('quiz'))
     return render_template('quiz/quiz_modif.html', form=form)
 
+
 @app.route('/quiz_delete/<int:id>', methods=['GET', 'POST'])
 def quiz_delete(id):
     quiz = Quiz.query.get(id)
     db_quiz.session.delete(quiz)
     db_quiz.session.commit()
     return redirect(url_for('quiz'))
+
 
 @app.route('/connexion', methods=['GET', 'POST'])
 def connexion():
@@ -150,14 +174,31 @@ def connexion():
 
     return render_template('connexion/connexion.html', form=form)
 
+
 @app.route('/deco')
 def deconnexion():
     logout_user()
     return redirect(url_for('index'))
 
+
 @login_manager.user_loader
 def load_user(id):
     return Utilisateur.query.get(int(id))
+
+
+@app.route('/nouvelutilisateur', methods=['GET', 'POST'])
+def nouvelutilisateur():
+    form = NouvelUtilisateurForm()
+    if form.validate_on_submit():
+        mot_de_passe_hash = generate_password_hash(form.mot_de_passe.data)
+        utilisateur = Utilisateur(
+        nom=form.login.data, mot_de_passe=mot_de_passe_hash, email=form.email.data, role=form.role.data)
+        db_utilisateur.session.add(utilisateur)
+        db_utilisateur.session.commit()
+        flash('L\'utilisateur a été créé avec succès.')
+        return redirect(url_for('index'))
+    return render_template('connexion/crea_user.html', title='Nouvel utilisateur', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
